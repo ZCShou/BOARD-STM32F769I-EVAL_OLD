1c816aa80887ebe74c4c7446edb2fd624f02feea
ZCShou <72115@163.com>
2022-12-01 14:44:28 +0800

    port u-boot-2022.10 to stm32f769i-eval board

--- u-boot-2022.10/arch/arm/include/asm/arch-stm32f7/stm32.h
+++ u-boot-2022.10/arch/arm/include/asm/arch-stm32f7/stm32.h
@@ -11,8 +11,8 @@
 
 static const u32 sect_sz_kb[CONFIG_SYS_MAX_FLASH_SECT] = {
 	[0 ... 3] =	32 * 1024,
-	[4] =		128 * 1024,
-	[5 ... 7] =	256 * 1024
+	[4] =		64 * 1024,
+	[5 ... 11] =	128 * 1024
 };
 
 #endif /* _ASM_ARCH_HARDWARE_H */
